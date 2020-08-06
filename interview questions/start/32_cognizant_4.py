305caac22ded108008ce4e83e8ff1c9868bdcbf4
Krishna Teja <packetcode@gmail.com>
2020-08-06 08:01:40 +0530

    Code updated

--- interview questions/start/32_cognizant_4.py
+++ interview questions/start/32_cognizant_4.py
@@ -1,2 +1,8 @@
 # write a program to print highest odd fibonacci number from a given range
-# eg: input : 5 , 250 output: 233
+# eg: input : 100 , 250 output: 233
+# ** important question - asked 5 to 6 times in coding interviews
+
+# write a fibonacci number series
+# take a temp varaible to store the odd fibonacii numbers
+# exit the loop after 250 (upper limit)
+# the last stored number in temp is the answer
