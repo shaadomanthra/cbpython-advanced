419558d97f1966540f4749904a814626c808e008
Krishna Teja <packetcode@gmail.com>
2020-08-06 08:11:13 +0530

    Code updated

--- day24/views/BarView.py
+++ day24/views/BarView.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from cv2 import cv2
 from pyzbar import pyzbar
 from PIL import Image
@@ -20,13 +21,13 @@ class BarView:
         self.label.grid(row=0,column=0,columns=3)
 
         b1 = Button(frame, text="start",command=self.webcam)
-        b1.grid(row=1,column=0,padx=5,pady=5)
+        b1.grid(row=1,column=0,padx=10,pady=10,sticky='nesw')
 
         b2 = Button(frame, text="stop", command = self.stop)
-        b2.grid(row=1,column=1,padx=5,pady=5)
+        b2.grid(row=1,column=1,padx=10,pady=10,sticky='nesw')
 
-        b3 = Button(frame, text="Capture",command = self.capture)
-        b3.grid(row=1,column=2,padx=5,pady=5)
+        b3 = Button(frame, text="Capture",command = self.capture,padx=10,pady=10)
+        b3.grid(row=1,column=2,padx=10,pady=10,sticky='nesw')
 
         window.mainloop()
 
@@ -60,8 +61,8 @@ class BarView:
                 cv2.putText(color, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (51, 51, 255), 2)
 
             # Convert the opencv image to tkinter image
-            image_array =Image.fromarray(color)
-            image_tk = ImageTk.PhotoImage(image_array)
+            self.image_array =Image.fromarray(color)
+            image_tk = ImageTk.PhotoImage(self.image_array)
 
             # update the image in the tkinter window
             self.label.config(image=image_tk)
@@ -72,10 +73,6 @@ class BarView:
                 self.label.after(10,self.start)
             else:
                 self.label.image = None
-
-
-
-
         except:
             print("Some error")
 
@@ -85,3 +82,5 @@ class BarView:
 
     def capture(self):
         print("Capture function")
+        self.image_array.save('images/1.jpg')
+        messagebox.showinfo("Image Capture","Image frame is captured")
