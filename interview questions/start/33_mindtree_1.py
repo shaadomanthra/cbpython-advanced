ab5bc824a6e377c91b9205473ad83e9f2d46016c
Krishna Teja <packetcode@gmail.com>
2020-08-07 07:02:03 +0530

    Code updated

---
+++ interview questions/start/33_mindtree_1.py
@@ -0,0 +1,9 @@
+# Python program to count Even and Odd numbers in a List
+
+# Input : a = 8, b = 4
+# Output : Yes
+# Binary representations of both
+# numbers have same 0s and 1s.
+#
+# Input : a = 4, b = 5
+# Output : No
---
+++ interview questions/start/34_mindtree_2.py
@@ -0,0 +1,10 @@
+# Program to Check if binary representations of two numbers are anagram
+
+# Input : a = 8, b = 4
+# Output : Yes
+# Binary representations of both
+# numbers have same 0s and 1s.
+#
+# Input : a = 4, b = 5
+# Output : No
+
