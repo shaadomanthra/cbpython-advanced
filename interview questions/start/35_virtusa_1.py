0b5d6a247d6fe6368cbb8a99f6d5cfc9db20cfff
Krishna Teja <packetcode@gmail.com>
2020-08-10 07:07:02 +0530

    Code updated

---
+++ interview questions/start/35_virtusa_1.py
@@ -0,0 +1,2 @@
+# Write a program to print a series of 20 numbers with even positions as prime numbers
+# starting from 3 and odd poisitions AP with common difference 5 and first number 10
---
+++ interview questions/start/36_zoho_1.py
@@ -0,0 +1,3 @@
+# write a program to print string removing vowels
+# eg: input : apple , output : ppl
+
