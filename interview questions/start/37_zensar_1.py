e8b9a95e0b1e18c869848533b7d7b6304ce15013
Krishna Teja <packetcode@gmail.com>
2020-08-11 16:01:24 +0530

    Code updated

---
+++ interview questions/start/37_zensar_1.py
@@ -0,0 +1,7 @@
+# Print first m multiples of n without using any loop in Python
+
+# Input : n = 2, m = 3
+# Output : 2 4 6
+#
+# Input : n = 3, m = 4
+# Output : 3 6 9 12
---
+++ interview questions/start/38_TechMahindra_1.py
@@ -0,0 +1,14 @@
+# Given the participants' score sheet for your University Sports Day,
+# you are required to find the runner-up score. You are given  scores.
+# Store them in a list and find the score of the runner-up.
+
+# input - The first line contains . The second line contains an array
+# of  integers each separated by a space.
+
+# eg: input
+# 5
+# 2 3 6 6 5
+
+# eg: output
+# 5
+
